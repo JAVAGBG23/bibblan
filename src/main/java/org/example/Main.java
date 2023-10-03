be7134b7df8b66358f978392efa16ae0b994e25c
Helena <helena@klarr.se>
2023-10-03 10:15:48 +0200

    bibblan example code...

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+       User user = new User("Helena");
+        //System.out.println(user.toString());
+
+        Book bookOne = new Book("JavaScript: The Good Parts", "Douglas Crockford", "Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad..", "1");
+        System.out.println(bookOne.toString());
+
+        //bookOne.available();
+    }
+}
